0bbb9bc3e2824c6a046f162f6922c0b28eebe586
Marc Van Olmen <marc@cameo.tv>
2015-04-17 17:49:13 -0400

    Initial Commit

---
+++ PodTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PodTest
+//
+//  Created by Van Olmen, Marc on 4/17/15.
+//  Copyright (c) 2015 Van Olmen, Marc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
